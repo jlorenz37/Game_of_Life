9521cf4cb69ba2f3f40e21cd72eec66129892d45
jlorenz37 <j37.lorenz@gmail.com>
2023-01-27 13:34:26 -0600

    1st commit. added class for board, cell

---
+++ cell.py
@@ -0,0 +1,13 @@
+class Cell:
+    
+    def init(self):
+        # todo
+
+    def set_dead(self):
+        # todo
+    
+    def set_alive(self):
+        # todo
+
+    def is_alive(self):
+        # todo
